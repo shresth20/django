7a3316771fa03e0e5f1cfec2e8b2493ed82c1be9
shresth20 <shresthakasera2020@gmail.com>
2025-05-26 22:13:11 +0530

    new django

---
+++ README.md
@@ -0,0 +1,4 @@
+# Django 
+Django – Full-stack framework with built-in ORM, authentication, and security features.
+
+ 
---
+++ learn_django/learn_django/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello welcome to Home page")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return render(request, 'website/about.html')
+
+def contact(request):
+    # return HttpResponse("Hello welcome ")
+    return render(request, 'website/contact.html')
+
